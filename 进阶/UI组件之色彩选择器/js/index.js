95b2531a32508ffa0493c113f14aba9ddb4c880d
Myth52125 <Myth52125@hotmail.com>
2017-02-28 21:18:11 +0800

    HSL

--- 进阶/UI组件之色彩选择器/js/index.js
+++ 进阶/UI组件之色彩选择器/js/index.js
@@ -22,28 +22,74 @@ var black=document.getElementById('black')
     var barH=barPointer.style.top
 
 function arrToBack(arr) {
-    return 'linear-gradient(to left, white,rgb('+parseInt(arr[0])+','+parseInt(arr[1])+','+parseInt(arr[2]) +'))'
+    return 'linear-gradient(to left, white,rgb('+parseFloat(arr[0])+','+parseFloat(arr[1])+','+parseFloat(arr[2]) +'))'
 
 }
 
 function arrToRgb(arr) {
-    return 'rgb('+parseInt(arr[0])+','+parseInt(arr[1])+','+parseInt(arr[2]) +')'
+    return 'rgb('+parseFloat(arr[0])+','+parseFloat(arr[1])+','+parseFloat(arr[2]) +')'
 }
+
+function rgbChangeIntohsl(rgb) {
+    //rgb为储存rgb颜色的长度为3的一维数组
+    var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+    var min = Math.min.apply(Array, [r, g, b]),
+        max = Math.max.apply(Array, [r, g, b]);
+    var h, s, l;
+    //计算h的值
+    if (max == min) {
+        h = 0;
+    }
+    else if (max == r && g >= b) {
+        h = 60 * (g - b) / (max - min);
+    }
+    else if (max == r && g < b) {
+        h = 60 * (g - b) / (max - min) + 360;
+    }
+    else if (max == g) {
+        h = 60 * (b - r) / (max - min) + 120;
+    }
+    else if (max == b) {
+        h = 60 * (r - g) / (max - min) + 240;
+    }
+    //计算l的值
+    l = (max + min) / 2;
+    //计算s的值
+    if (l == 0 || max == min) {
+        s = 0;
+    }
+    else if (l > 0 && l <= 0.5) {
+        s = (max - min) / (2 * l);
+    }
+    else if (l > 0.5) {
+        s = (max - min) / (2 - 2 * l);
+    }
+
+    return [Math.round(h), Math.round(s * 100) / 100, Math.round(l * 100) / 100];
+
+}
+
 
 //用于显示数据的RGBHSL
 function showData(arrRgb) {
-    R.value=parseInt(arrRgb[0])
-    G.value=parseInt(arrRgb[1])
-    B.value=parseInt(arrRgb[2])
-
+    R.value=parseFloat(arrRgb[0])
+    G.value=parseFloat(arrRgb[1])
+    B.value=parseFloat(arrRgb[2])
+    var arrHsl=rgbChangeIntohsl(arrRgb)
+    H.value=parseFloat(arrHsl[0])
+    L.value=parseFloat(arrHsl[1])
+    S.value=parseFloat(arrHsl[2])
+    console.log(arrHsl)
 }
 
 
 function getBarColor() {
     var barH=barPointer.style.top
     var spa=400/6
-    var remain=parseInt(barH)%spa
-    var item=Math.floor(parseInt(barH)/spa)
+    var remain=parseFloat(barH)%spa
+    var item=Math.floor(parseFloat(barH)/spa)
 
     console.log('item:'+item+'    remain:'+remain)
 
@@ -120,7 +166,7 @@ function colorChange(arr,x,y) {
         arr[i]=arr[i]+(255-arr[i])*(x)/400
         arr[i]=arr[i]-arr[i]*y/400
     }
-console.log(arr)
+// console.log(arr)
     return arr
 }
 
@@ -129,7 +175,7 @@ function getColorShowColor() {
 
     var barRgb=getBarColor()
 
-    var newColor=colorChange(barRgb,parseInt(colorPointer.style.left),parseInt(colorPointer.style.top))
+    var newColor=colorChange(barRgb,parseFloat(colorPointer.style.left),parseFloat(colorPointer.style.top))
     showData(newColor)
     return newColor
 }
